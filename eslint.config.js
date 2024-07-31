58738a796572a9fd7f382a23c005edf264398c9a
Rafli Malik <Rafli.Malik@gmail.com>
2024-07-31 10:13:01 +0700

    chore: add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import jsPlugin from '@eslint/js';
+import prettierPlugin from 'eslint-config-prettier';
+
+export default [
+  prettierPlugin,
+  jsPlugin.configs.recommended,
+  ...eslintPluginAstro.configs['jsx-a11y-recommended'],
+  {
+    ignores: ['node_modules', '*.mjs'],
+  },
+];
