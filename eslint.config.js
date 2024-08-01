06c894a91a36ff1aee90cda618a0844b3777f81b
Rafli Malik <Rafli.Malik@gmail.com>
2024-08-01 09:28:33 +0700

    chore(deps): update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,9 @@ import jsPlugin from "@eslint/js";
 import prettierPlugin from "eslint-config-prettier";
 
 export default [
-  prettierPlugin,
   jsPlugin.configs.recommended,
   ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+  prettierPlugin,
   {
     ignores: ["node_modules", "*.mjs"],
   },
