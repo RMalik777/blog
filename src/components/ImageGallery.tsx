e9937c8dce3aebe761cf9ee8c0d0af9ced01d428
Rafli Malik <Rafli.Malik@gmail.com>
2024-08-20 10:06:22 +0700

    feat: add autoplay for carousel

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,6 +1,7 @@
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { urlFor } from "@/utils/image";
 import type { ImageAsset } from "@sanity/types";
+import Autoplay from "embla-carousel-autoplay";
 
 interface Props {
   images: ImageAsset[] | undefined;
@@ -9,6 +10,11 @@ interface Props {
 export function ImageGallery({ images }: Readonly<Props>) {
   return images ? (
     <Carousel
+      plugins={[
+        Autoplay({
+          delay: 5000,
+        }),
+      ]}
       opts={{
         loop: true,
       }}>
