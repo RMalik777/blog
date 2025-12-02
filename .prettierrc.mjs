1b7cfbbfefeba9d826f2ccbbf5f6c70c20a2ae97
Rafli Malik <Rafli.Malik@gmail.com>
2025-12-02 15:01:21 +0700

    chore: update component

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,11 +1,12 @@
 // .prettierrc.mjs
-/** @type {import("prettier").Config} */
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
 	useTabs: true,
 	tabWidth: 2,
 	printWidth: 120,
 	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
 	tailwindFunctions: ["clsx", "cn", "cva"],
+	tailwindStylesheet: "./src/styles/global.css",
 	overrides: [
 		{
 			files: "*.astro",
