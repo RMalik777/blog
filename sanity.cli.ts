045fe25c194941be2b4b288a40c6e6816823aa8b
Rafli Malik <Rafli.Malik@gmail.com>
2025-12-09 15:24:01 +0700

    chore: add sanity cli

---
+++ sanity.cli.ts
@@ -0,0 +1,20 @@
+/**
+ * Sanity CLI Configuration
+ * This file configures the Sanity CLI tool with project-specific settings
+ * and customizes the Vite bundler configuration.
+ * Learn more: https://www.sanity.io/docs/cli
+ */
+
+import { defineCliConfig } from "sanity/cli";
+
+const projectId = process.env.PUBLIC_SANITY_STUDIO_PROJECT_ID;
+const dataset = process.env.PUBLIC_SANITY_STUDIO_DATASET;
+
+export default defineCliConfig({
+	api: {
+		projectId,
+		dataset,
+	},
+	studioHost: "https://blog.raflimalik.com/admin",
+	autoUpdates: true,
+});
